1a08175552515156ea89d279a80d22499b78a4d1
Michael <norritec@users.noreply.github.com>
2016-11-01 11:30:17 -0500

    Initial commit

---
+++ app/src/main/java/norritec/github/io/todolist/db/TaskContract.java
@@ -0,0 +1,17 @@
+package norritec.github.io.todolist.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by administrator on 6/18/16.
+ */
+public class TaskContract {
+    public static final String DB_NAME = "com.norritec.github.io.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
